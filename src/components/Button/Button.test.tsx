d5de613511c172cf6b7a370cb936e5dd35be9d50
ggoleta <rogerio_contato@hotmail.com>
2022-03-07 11:38:46 -0300

    tests

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -3,14 +3,44 @@ import Button from './'
 
 describe('Button Component', () => {
 
-  it('should render Button correctly', () => {
+  test('render Button correctly', () => {
     render (
-      <Button color='primary' isLoading>
-        Text
+      <Button color='primary'>
+        Button 1
       </Button>
     )
   
-    expect(screen.getByText('Text')).toBeInTheDocument()
+    expect(screen.getByText('Button 1')).toBeInTheDocument()
+  })
+
+  test('background color primary', () => {
+    render (
+      <Button color='primary'>
+        Button 1
+      </Button>
+    )
+  
+    expect(screen.getByText('Button 1')).toHaveStyle('background-color: #026C00')
+  })
+
+  test('background color secondary', () => {
+    render (
+      <Button color='secondary'>
+        Button 1
+      </Button>
+    )
+  
+    expect(screen.getByText('Button 1')).toHaveStyle('background-color: #F7C346')
+  })
+
+  test('component disabled', () => {
+    render (
+      <Button color='secondary' disabled>
+        Button 1
+      </Button>
+    )
+  
+    expect(screen.getByText('Button 1')).toHaveStyle('background-color: #FFE58D')
   })
 
 })
